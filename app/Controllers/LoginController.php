64ed81c62fa872bd4933eb6420f94ef04c819fda
KartikDaGreat <108451750+KartikDaGreat@users.noreply.github.com>
2023-12-19 13:42:32 +0530

    first commit final login page

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,38 @@
+<?php 
+namespace App\Controllers;
+use App\Models\LoginModel;
+use CodeIgniter\Controller;
+
+class LoginController extends Controller
+{
+    private $login = '' ;
+    public function __construct(){
+      
+        $this->login = new LoginModel();       
+    }
+    
+    // show login form
+    public function index()    {  
+        $session = session();  
+        $session->setFlashdata('msg', '');
+        return view('login');
+    }      
+
+    //check user is exist or not
+    public function login(){
+        $session = session();
+        
+        $data = array('user_name'=>$this->request->getVar('user_id'),'password'=>$this->request->getVar('password'));
+        $user =  $this->login->where($data);
+        $rows = $user->countAllResults();
+        if($rows==1)
+        {
+            return view('success');
+        }
+        else
+        {
+            $session->setFlashdata('msg', 'Invalid User');
+            return view('login');
+        } 
+     }
+}
